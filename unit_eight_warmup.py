cd8a1afceeb441a41afd25412ba071d5d562b9e5
Min Woo <minwoo@iMac5.local>
2018-11-07 13:40:35 -0500

    tempconverter

---
+++ unit_eight_warmup.py
@@ -0,0 +1,19 @@
+import tkinter
+
+root = tkinter.Tk()
+
+root.title("tempconverter")
+
+f_label = tkinter.Label(root, text="degrees F:")
+f_label.grid(row=1, column=1)
+
+f_entry = tkinter.Entry(root)
+f_entry.grid(row=1, column=2)
+
+c_label = tkinter.Label(root, text="degrees C:")
+c_label.grid(row=2, column=1)
+
+c_temp_label = tkinter.Label(root, text="")
+c_temp_label.grid(row=2, column=2)
+
+root.mainloop()
