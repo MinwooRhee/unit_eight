bd4bda2e657c040f3dde5d338bb26396ba33a61d
Min Woo <minwoo@iMac5.local>
2018-11-08 12:26:24 -0500

    StringVar added

--- unit_eight_warmup.py
+++ unit_eight_warmup.py
@@ -2,19 +2,21 @@ import tkinter
 
 root = tkinter.Tk()
 
+fahrenheit = tkinter.StringVar()
+celsius = tkinter.StringVar()
+
 root.title("tempconverter")
 
 f_label = tkinter.Label(root, text="degrees F:")
 f_label.grid(row=1, column=1)
 
-f_entry = tkinter.Entry(root)
+f_entry = tkinter.Entry(root, textvariable=fahrenheit)
 f_entry.grid(row=1, column=2)
 
 c_label = tkinter.Label(root, text="degrees C:")
 c_label.grid(row=2, column=1)
 
-c_temp_label = tkinter.Label(root, text="")
+c_temp_label = tkinter.Label(root, textvariable=celsius)
 c_temp_label.grid(row=2, column=2)
 
-# End the program
 root.mainloop()
